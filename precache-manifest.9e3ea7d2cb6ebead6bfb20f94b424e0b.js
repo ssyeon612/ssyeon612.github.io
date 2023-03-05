4a3ddb7e911bbc104636c562574b88c3acf65bff
ssyeon612 <ssyeon612@gmai.com>
2023-03-05 17:55:18 +0900

    Updates

---
+++ precache-manifest.9e3ea7d2cb6ebead6bfb20f94b424e0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3245287ef98e3cff029d37d25f6ef1eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536da8b75bd367cd66e3",
+    "url": "/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "6c4b9b133ad983ea2f34",
+    "url": "/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "536da8b75bd367cd66e3",
+    "url": "/static/js/2.a239c11b.chunk.js"
+  },
+  {
+    "revision": "ab4784922d7f45cc6cfd5a83588623bb",
+    "url": "/static/js/2.a239c11b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4b9b133ad983ea2f34",
+    "url": "/static/js/main.598c95e6.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
